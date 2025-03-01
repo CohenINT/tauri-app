ed6ee2c671037ad173a41d013171280ade1b4fec
CohenINT <moshe22cohen@gmail.com>
2025-03-01 23:40:05 +0200

    addedd basic tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
- import './styles.css';
+import './styles.css';
 
 function App() {
 
 
- 
+
 
   const clickHandler = (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     console.log(`${ev.clientX}`);
@@ -11,10 +11,21 @@ function App() {
   };
 
   return (
-    <div className="container flexs bg-red-500  bg-red-500 w-full h-full bg-red-500">
-      <h1 className="backdrop-blur-sm">Welcome to Tauri!</h1>
-    
-      <button className="bg-red-500" onClick={clickHandler}>click</button>
+    <div className="flex w-full flex-col justify-center items-center ">
+      <div className='flex flex-col justify-center items-center bg-green-200 p-4 rounded-2xl mt-12'>
+        <h1 className=" ">Welcome to Tauri!</h1>
+        <h4>Pick a Tab!</h4>
+      </div>
+      <div className='tabs_gallery flex flex-row  w-[50%] justify-evenly p-5 rounded-3xl  bg-orange-100 '>
+
+        {
+          //TODO: each button make a Tab and reroute to different page accordingly.
+        }
+        <button  className='block  font-bold text-7xl'>Snoring Monitoring</button>
+        <button className='block  font-bold text-7xl'>Looper duper!</button>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/Components/Tab.tsx
@@ -0,0 +1,11 @@
+
+
+export interface ITabProps {
+    classes: string;
+    rerouteSrc: string;
+}
+export const Tab: React.FC<ITabProps> = () => {
+
+
+    return (<></>);
+};
